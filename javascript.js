43681e11fc7640e1e62f4a37863c306448830889
Andrea Lauro <andrea.lauro94@gmail.com>
2023-05-23 21:03:29 +0200

    commit

---
+++ javascript.js
@@ -0,0 +1,35 @@
+const ApiUrl = "https://api.pexels.com/v1";
+const APIKey = "Nzko9a1jt52L7mcLauKoRCRApQDhFHcIlaMI1clvwRxXYz1RTG0TLWnX";
+let main = document.getElementById("main")
+
+
+//! 1) fare chiamata ajax
+
+const getAlbum = () =>{
+    fetch(ApiUrl + "/search?query=TRAVEL", {
+        headers: {
+             Authorization: APIKey }
+    })
+   .then((res) => {
+    return res.json()
+   })
+   .then((res2) => {
+    console.log(res2.photos)
+   })
+   .catch((err) =>{console.log(err)});
+
+    
+}
+getAlbum();
+
+//! 2) funzione creare un dom con gli oggetti degli api
+
+function albumCard(input) {
+    console.log(input);
+    for ( const photo of input){
+        img = document.createElement("img");
+        img.src = photo.src.medium;
+        main.appendChild(img);
+    }
+
+}
