67c553dd889b9d850972a26ada1adfd553484f98
Andrea Lauro <andrea.lauro94@gmail.com>
2023-05-25 19:04:19 +0200

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -37,6 +37,7 @@ const getAlbum = () => {
     })
         .then((res2) => {
             albumCard(res2.photos)
+            console.log(res2)
         })
         .catch((err) => {console.error("Hai un errore: " + err)});
 }
@@ -44,35 +45,53 @@ const getAlbum = () => {
 
 // //! 2) funzione creare un dom con gli oggetti degli api
 
-// const albumCard = (photos) => {
+const albumCard = (photos) => {
+    let row = document.createElement("div")
+    row.classList.add("row")
+    albumContainer.appendChild(row)
+   
     
-//    photos.forEach(photo => {
-//     const img = document.createElement("img");
-//         img.src = photo.src.small;
-//         card.appendChild(img)
-        
-//    });   
+    
+   photos.forEach(photo => {
+    let col = document.createElement("div")
+    col.classList.add("col-md-3")
+    let card = document.createElement("card")
+    card.classList.add("card")
+
+    let img = document.createElement("img");
+        img.src = photo.src.small;
+        card.appendChild(img)
+        col.appendChild(card)
+        row.appendChild(col)
+   });   
     
 
-// }
+}
+
+
+
+
+
+
+
 
 
 //problema 1) funzione creare un DOM con gli oggetti degli API
 
-const albumCard = (photos) => {
-    console.log(photos)
-    photos = photos.filter (item => {return item.width <= 4000} )
-    const domElements = photos.map(photo => {
-      //  console.log(photo);
-      const img = document.createElement("img");
-      img.src = photo.src.small;
-      //albumContainer.appendChild(img);
-      return img
-    });
-    console.log(domElements)
-    //albumContainer.append(domElements[0], domElements[1], domElements[2])
-    albumContainer.append(...domElements)
-}
+// const albumCard = (photos) => {
+//     console.log(photos)
+//     photos = photos.filter (item => {return item.width <= 4000} )
+//     const domElements = photos.map(photo => {
+//       //  console.log(photo);
+//       const img = document.createElement("img");
+//       img.src = photo.src.small;
+//       //albumContainer.appendChild(img);
+//       return img
+//     });
+//     console.log(domElements)
+//     //albumContainer.append(domElements[0], domElements[1], domElements[2])
+//     albumContainer.append(...domElements)
+// }
 
 
 
